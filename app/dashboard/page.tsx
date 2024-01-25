d096dcfa7321c91ce9ff0cfc74269ce1138abe6f
carlitossaul <carlitossaul.h@gmail.com>
2024-01-25 09:16:03 -0300

    navbar designed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,16 +2,28 @@
 import React, { useEffect, useState } from "react";
 import PrivateRoute from "@/app/components/PrivateRoute/PrivateRoute";
 import { useAuthContext } from "@/app/contexts/AuthContext";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 function Dashboard() {
   const { user } = useAuthContext();
 
   return (
     <PrivateRoute>
-      <main>
-        <h1>Oi {user} !</h1>
-        <h1>Voce ta em Dashboard</h1>
-      </main>
+      <nav className={styles.nav}>
+        <Image
+          src="/images/Logo-preto.png"
+          alt="logo"
+          width={180}
+          height={55}
+        ></Image>
+        <Link href={"/dashboard/gerenciar-usuarios"}>Gerenciar Usuários</Link>
+        <Link href={"/dashboard/gerenciar-tickets"}>Gerenciar Tickets</Link>
+        <Link href={"/dashboard/gerenciar-dashboards"}>
+          Gerenciar Dashboards
+        </Link>
+      </nav>
     </PrivateRoute>
   );
 }
