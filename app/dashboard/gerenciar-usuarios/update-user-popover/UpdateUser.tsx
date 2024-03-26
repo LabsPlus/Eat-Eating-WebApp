10a4da91e705193f191a8ecc6b29638a40ace41e
Tobias <tobiasramossilva@gmail.com>
2024-03-26 13:36:49 -0300

    making the users alphabetical order and numbering them

--- app/dashboard/gerenciar-usuarios/update-user-popover/UpdateUser.tsx
+++ app/dashboard/gerenciar-usuarios/update-user-popover/UpdateUser.tsx
@@ -271,7 +271,7 @@ const UpdateUser: React.FC = () => {
                 <img src={formUpdate.picture} alt="Foto do usuário" />
               ) : (
                 <img
-                  src="https://s3-alpha-sig.figma.com/img/15e6/00b9/f4886412d415517e0fbef2099bad9f6d?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fe3QsdCcr6bLt3g3q4c-g-AEpkFgx9VS1810bMEE-UvvOE5VVBfsT5DfA518t8lyD94Kwz0pljcGtBESmCoZwD6dv4CslVcr~PiT72uNbY463~MrqEGBsF321u8AXCFbz88PAeP2d-0qcW2vdswVFUthfJ8Oup00DeKc6pS3hOZWo4Y5oyrt02QsVPTlj6MqCLRUrGlfw1MofHiVVdW6R18VF4J5eaFVtNoR-CP3LsojL2Yflxby1MyDwQ1lp~BnoIJKwjCycG9IuPxjLPi0RL-W9q8jJdCBM1l10ISd6A4Y4Szbkxd5daRgP7zOO~reHGpDRW34tC7MIg0aYPQTDQ__"
+                  src="https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Free-Image.png"
                   alt="Foto do usuário"
                 />
               )}
