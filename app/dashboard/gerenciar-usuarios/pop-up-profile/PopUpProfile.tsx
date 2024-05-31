e103e03d163da17ae916c078d834db8991d3e561
carlitossaul <carlitossaul.h@gmail.com>
2024-05-31 13:56:10 -0300

    chore: Update PopUpProfile component styling

--- app/dashboard/gerenciar-usuarios/pop-up-profile/PopUpProfile.tsx
+++ app/dashboard/gerenciar-usuarios/pop-up-profile/PopUpProfile.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Button, Modal } from "antd";
-import { LogOut, User, Mail } from 'lucide-react';
+import { LogOut, User, Mail } from "lucide-react";
 import axios from "axios";
 
 import Image from "next/image";
@@ -16,32 +16,54 @@ const PopUpProfile = () => {
 
   const getUsers = async () => {
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/user/list-all-users`,
+      `${process.env.NEXT_PUBLIC_API_URL}/user/list-all-users`
     );
 
-    return response.data
-  }
+    return response.data;
+  };
 
   const getUserByEmail = async () => {
-    const users = await getUsers()
+    const users = await getUsers();
 
     // const getUserLogged = users.filter((userSelect: any) => user === userSelect.user.loginUser.email)
 
-    const getUserLogged = users.forEach((userSelect: any) => console.log(userSelect.user))
+    const getUserLogged = users.forEach((userSelect: any) =>
+      console.log(userSelect.user)
+    );
 
-    console.log(getUserLogged)
-    console.log(user)
-  }
+    console.log(getUserLogged);
+    console.log(user);
+  };
 
   useEffect(() => {
-    getUserByEmail()
-  }, [])
+    getUserByEmail();
+  }, []);
 
   return (
     <Modal
       open={true}
       closable={false}
-      className={styles.popUpUser}
+      title={
+        <div className={styles.containerPopUp}>
+          <Image
+            src="https://avatars.githubusercontent.com/u/64218964?v=4"
+            alt="image"
+            width={72}
+            height={72}
+            className={styles.imgPopUp}
+          />
+          <div className={styles.infoUser}>
+            <h2>Amanda Oliveira</h2>
+            <p>SEO Expert</p>
+            <p>
+              <span>
+                <Mail className={styles.iconsPopUp} />
+              </span>
+              info@modernize.com
+            </p>
+          </div>
+        </div>
+      }
       footer={
         <div className={styles.containerButtonsPopUp}>
           <Button
@@ -51,34 +73,14 @@ const PopUpProfile = () => {
             Meu perfil
           </Button>
           <Button
-            icon={<LogOut className={styles.iconsPopUp} />} 
+            icon={<LogOut className={styles.iconsPopUp} />}
             className={styles.buttonsPopUp}
           >
             Sair
           </Button>
         </div>
       }
-    >
-      <div className={styles.containerPopUp}>
-        <Image 
-          src="https://avatars.githubusercontent.com/u/64218964?v=4" 
-          alt="image" 
-          width={72} 
-          height={72}
-          className={styles.imgPopUp} 
-        />
-        <div className={styles.infoUser}>
-          <h2>Amanda Oliveira</h2>
-          <p>SEO Expert</p>
-          <p>
-            <span>
-              <Mail className={styles.iconsPopUp} />
-            </span>
-            info@modernize.com
-          </p>
-        </div>
-      </div>
-    </Modal>
+    ></Modal>
   );
 };
 
